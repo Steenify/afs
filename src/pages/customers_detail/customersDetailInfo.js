3e0fa9a6db41493b6b1d41174f4c2c8cb23a4e4b
Huu Nghia <nghiahh@steenify.com>
2020-08-03 10:47:30 +0700

    Updated UI for customer/artist contact info

--- src/pages/customers_detail/customersDetailInfo.js
+++ src/pages/customers_detail/customersDetailInfo.js
@@ -46,15 +46,64 @@ const CustomerDetailInfo = (props) => {
         <div className='row'>
           <div className='col-6'>
             <div className='box__sub_title mb-2'>Contact</div>
-            <div className='box__link'>{customer?.email}</div>
-            <div className='box__link'>{customer?.fbUrl}</div>
-            <div className='box__link'>{customer?.igUrl}</div>
-            <div className='box__link'>{customer?.snapChatUrl}</div>
-            <div className='box__link'>{customer?.linkedUrl}</div>
-            <div className='box__link'>{customer?.twitterUrl}</div>
-            <div className=''>
-              {customer?.phonePrefix} {customer?.phoneNumber}
-            </div>
+            {customer?.email && <p className='mb-1'>Email: {customer?.email}</p>}
+            {customer?.phoneNumber && <p className='mb-1'>Phone: {customer?.phoneNumber}</p>}
+            {customer?.fbChat && (
+              <p className='mb-1'>
+                Facebook Chat: &nbsp;
+                <a target='_blank' rel='noopener noreferrer' href={`${customer?.fbChat}`}>
+                  Link
+                </a>
+              </p>
+            )}
+            {customer?.mailChain && (
+              <p className='mb-1'>
+                Mail Chain: &nbsp;
+                <a target='_blank' rel='noopener noreferrer' href={`${customer?.mailChain}`}>
+                  Link
+                </a>
+              </p>
+            )}
+            {customer?.fbUrl && (
+              <p className='mb-1'>
+                Facebook: &nbsp;
+                <a target='_blank' rel='noopener noreferrer' href={`${customer?.fbUrl}`}>
+                  Link
+                </a>
+              </p>
+            )}
+            {customer?.igUrl && (
+              <p className='mb-1'>
+                Instagram: &nbsp;
+                <a target='_blank' rel='noopener noreferrer' href={`${customer?.igUrl}`}>
+                  Link
+                </a>
+              </p>
+            )}
+            {customer?.snapChatUrl && (
+              <p className='mb-1'>
+                SnapChat: &nbsp;
+                <a target='_blank' rel='noopener noreferrer' href={`${customer?.snapChatUrl}`}>
+                  Link
+                </a>
+              </p>
+            )}
+            {customer?.linkedUrl && (
+              <p className='mb-1'>
+                LinkedIn: &nbsp;
+                <a target='_blank' rel='noopener noreferrer' href={`${customer?.linkedUrl}`}>
+                  Link
+                </a>
+              </p>
+            )}
+            {customer?.twitterUrl && (
+              <p className='mb-1'>
+                Twitter: &nbsp;
+                <a target='_blank' rel='noopener noreferrer' href={`${customer?.twitterUrl}`}>
+                  Link
+                </a>
+              </p>
+            )}
           </div>
           <div className='col-6'>
             <div className='box__sub_title mb-2'>Address</div>
