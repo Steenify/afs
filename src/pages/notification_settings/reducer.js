c4b17b61ac53e8c7c792dea9e53a0644a274977e
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2020-08-18 18:04:17 +0700

    show loading when update notification settings

--- src/pages/notification_settings/reducer.js
+++ src/pages/notification_settings/reducer.js
@@ -1,3 +1,6 @@
+import update from 'immutability-helper';
+import { GET_NOTIFICATION_ACTIONS, GET_NOTIFICATION_ACTION, UPDATE_NOTIFICATION_ACTION, GET_RECIPIENTS } from './action';
+
 const initialState = {
   ui: {
     isLoading: false,
@@ -7,6 +10,17 @@ const initialState = {
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+    case UPDATE_NOTIFICATION_ACTION.PENDING: {
+      return update(state, {
+        ui: { isLoading: { $set: true } },
+      });
+    }
+    case UPDATE_NOTIFICATION_ACTION.SUCCESS:
+    case UPDATE_NOTIFICATION_ACTION.ERROR: {
+      return update(state, {
+        ui: { isLoading: { $set: false } },
+      });
+    }
     default:
       return state;
   }
