49178169d43a81cb5761df162f714b9ad10709c4
Huu Nghia <nghiahh@steenify.com>
2020-08-19 12:14:26 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29b49787e3771a3b197ab6e6a93c8542.js"
+  "/precache-manifest.1d8f34d70b7a862795b57448b0b52e5a.js"
 );
 
 self.addEventListener('message', (event) => {
