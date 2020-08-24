883b2e97778650b94eba509d6cfdface0c877499
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2020-08-24 18:17:14 +0700

    update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,57 @@
 // config-overrides.js
-const { useBabelRc, override, addWebpackPlugin } = require('customize-cra');
 const MergeJsonWebpackPlugin = require('merge-jsons-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
-module.exports = override(
-  useBabelRc(),
-  addWebpackPlugin(
-    new MergeJsonWebpackPlugin({
-      output: {
-        groupBy: [
-          {
-            pattern: 'public/locales/en/*.json',
-            fileName: '../public/locales/en.json',
+module.exports = {
+  webpack: (config, env) => {
+    config.plugins = [
+      ...config.plugins,
+      new MergeJsonWebpackPlugin({
+        debug: env !== 'production',
+        output: {
+          groupBy: [
+            {
+              pattern: 'public/locales/en/*.json',
+              fileName: 'locales/en.json',
+            },
+            {
+              pattern: 'public/locales/vi/*.json',
+              fileName: 'locales/vi.json',
+            },
+          ],
+        },
+      }),
+    ];
+
+    if (env === 'production') {
+      config.optimization.minimizer = [
+        ...config.optimization.minimizer,
+        new TerserWebpackPlugin({
+          parallel: true,
+          sourceMap: false,
+          extractComments: false,
+          terserOptions: {
+            compress: {
+              comparisons: false,
+              booleans: true,
+              pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn'],
+            },
+            warnings: false,
+            mangle: true,
+            output: {
+              comments: false,
+              ascii_only: true,
+            },
           },
-          {
-            pattern: 'public/locales/vi/*.json',
-            fileName: '../public/locales/vi.json',
-          },
-        ],
-      },
-    }),
-  ),
-);
+        }),
+      ];
+    }
+
+    return config;
+  },
+  devServer: (config, env) => {
+    config.writeToDisk = true;
+
+    return config;
+  },
+};
