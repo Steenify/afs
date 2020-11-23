6773e6779ad71fa5e61d1d60b1be5d9ac70fecd6
Hoang Nguyen <>
2020-11-23 10:05:07 +0700

    apply ui with permission

--- src/pages/orders_detail/assignedBoxModal.js
+++ src/pages/orders_detail/assignedBoxModal.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { toast } from 'react-toastify';
-import { isEmpty } from 'lodash';
 
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
 import Button from 'components/common/button';
-import ListProduct from 'components/layout/ListProduct';
-import Dropbox from 'components/common/dropbox';
-import OrderBudget from './orderBudget';
-import OrderAssignedBox from './orderAssignedBox';
 import { ReactComponent as CloseIcon } from 'assets/img/close.svg';
 import { ReactComponent as RightRectangle } from 'assets/img/right_rectangle.svg';
 
 import { updateShowAssignedBoxAction, ASSIGNED_MODAL_KEYs } from './actions';
 import { formatMoney } from 'utils';
+import { PERMITTIONS_CONFIG, mapRoles } from 'configs';
 
-const AssignedBoxModal = ({ order, isOpen, updateShowAssignedBoxAction }) => {
+const AssignedBoxModal = ({ order, isOpen, permissions = [], authorities = [], updateShowAssignedBoxAction }) => {
   const { artistBudgets = [], assignedTo = {}, budget = 0 } = order || {};
   const { CHANGE_ARTIST, CHANGE_BUDGET, BUDGET_HISTORY, INCREASE_BUDGET, DECREASE_BUDGET } = ASSIGNED_MODAL_KEYs;
-
+  const { MODIFY_BUDGET, ADJUST_BUDGET, ASSIGN_BOOKING, GET_BUDGET_CHANGE_LOG } = PERMITTIONS_CONFIG;
   const previousArtists = artistBudgets.filter((item) => item?.artist?.id !== assignedTo?.id);
+
+  const canModifyBudget = permissions.includes(MODIFY_BUDGET) || authorities.includes(mapRoles.ROLE_ADMIN);
+  const canAssign = permissions.includes(ASSIGN_BOOKING);
+  const canAdjustBudget = permissions.includes(ADJUST_BUDGET);
+  const canViewBudgetHistory = permissions.includes(GET_BUDGET_CHANGE_LOG);
+
   const toggle = () => updateShowAssignedBoxAction('');
   return (
     <Modal isOpen={isOpen} toggle={toggle} fade={false} size='lg' className='modal-dialog-centered  modal-no-border order_detail__assignedModal'>
@@ -38,57 +38,62 @@ const AssignedBoxModal = ({ order, isOpen, updateShowAssignedBoxAction }) => {
             <button
               className='assignedModal__action'
               onClick={() => {
-                updateShowAssignedBoxAction(CHANGE_ARTIST);
+                if (canAssign) {
+                  updateShowAssignedBoxAction(CHANGE_ARTIST);
+                }
               }}>
               <div className='assignedModal__action__container'>
                 <span className='left text-black'>Current Artist:</span>
                 <strong className='right text-black'>{assignedTo?.fullName}</strong>
-                <RightRectangle width='12px' height='12px' />
+                {canAssign && <RightRectangle width='12px' height='12px' />}
               </div>
             </button>
             <button
               className='assignedModal__action'
               onClick={() => {
-                updateShowAssignedBoxAction(CHANGE_BUDGET);
+                if (canModifyBudget) {
+                  updateShowAssignedBoxAction(CHANGE_BUDGET);
+                }
               }}>
               <div className='assignedModal__action__container'>
                 <span className='left text-black'>Budget:</span>
                 <strong className='right text-black'>{formatMoney(budget)}</strong>
-                <RightRectangle width='12px' height='12px' />
+                {canModifyBudget && <RightRectangle width='12px' height='12px' />}
               </div>
             </button>
           </div>
 
           <div className={`assignedModal__content`}>
-            <div>
+            {canAdjustBudget && (
+              <div>
+                <button
+                  className=''
+                  onClick={() => {
+                    updateShowAssignedBoxAction(INCREASE_BUDGET);
+                  }}>
+                  <div className=''>Increase Budget</div>
+                </button>
+                <button
+                  className=''
+                  onClick={() => {
+                    updateShowAssignedBoxAction(DECREASE_BUDGET);
+                  }}>
+                  <div className=''>Decrease Budget</div>
+                </button>
+              </div>
+            )}
+            {canViewBudgetHistory && (
               <button
                 className=''
                 onClick={() => {
-                  updateShowAssignedBoxAction(INCREASE_BUDGET);
+                  updateShowAssignedBoxAction(BUDGET_HISTORY);
                 }}>
-                <div className=''>Increase Budget</div>
+                <div className=''>Budget History</div>
               </button>
-              <button
-                className=''
-                onClick={() => {
-                  updateShowAssignedBoxAction(DECREASE_BUDGET);
-                }}>
-                <div className=''>Decrease Budget</div>
-              </button>
-            </div>
-            <button
-              className=''
-              onClick={() => {
-                updateShowAssignedBoxAction(BUDGET_HISTORY);
-              }}>
-              <div className=''>Budget History</div>
-            </button>
+            )}
           </div>
         </ModalBody>
         <ModalFooter>
-          {/* <Button color='secondary' onClick={toggle}>
-            Cancel
-          </Button> */}
           <Button color='primary' onClick={toggle}>
             Done
           </Button>
@@ -114,8 +119,10 @@ const AssignedBoxModal = ({ order, isOpen, updateShowAssignedBoxAction }) => {
     </Modal>
   );
 };
-const mapStateToProps = ({ orderDetail }) => ({
+const mapStateToProps = ({ orderDetail, auth }) => ({
   isOpen: orderDetail.ui.assignedBox.currentShow === ASSIGNED_MODAL_KEYs.ASSIGNED,
+  permissions: auth?.data?.accountInfo?.permissions || [],
+  authorities: auth?.data?.accountInfo?.authorities || [],
 });
 
 const mapDispatchToProps = {
